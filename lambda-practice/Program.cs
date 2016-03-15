de7b03c5de8dccedef6d43615eddfe656008fb49
Antonio <toniohdez43@users.noreply.github.com>
2016-03-14 17:20:08 -0700

    1st commit

--- lambda-practice/Program.cs
+++ lambda-practice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using lamda_practice.Data;
 using System;
+using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
 
@@ -9,28 +10,69 @@ namespace lambda_practice
     {
         static void Main(string[] args)
         {
-
             using (var ctx = new DatabaseContext())
             {
-                //1. Listar todos los empleados cuyo departamento tenga una sede en Chihuahua
-               
+                Console.Write("1. Empleados cuyo departamento tenga una sede en Chihuahua\n");
+                System.Threading.Thread.Sleep(1000);
+                var query = ctx.Employees
+                    .Where(employee => employee.City.Name == "Chihuahua");
 
-                //2. Listar todos los departamentos y el numero de empleados que pertenezcan a cada departamento.
-               
+                foreach (var worker in query)
+                {
+                    Console.WriteLine(worker.FirstName + ' ' + worker.LastName);
+                }
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("2. Departamentos y el numero de empleados que pertenezcan a cada departamento.\n");
+                System.Threading.Thread.Sleep(1000);
+                var query2 = ctx.Employees
+                    .GroupBy(employee => employee.Department.Name)
+                    .Select(res => new { Name = res.Key, Count = res.Count() });
 
-                //3. Listar todos los empleados remotos. Estos son los empleados cuya ciudad no se encuentre entre las sedes de su departamento.
-             
+                foreach (var Department in query2)
+                {
+                    Console.WriteLine("Nombre Departamento: {0},  Numero de Empleados: {1}",
+                    Department.Name, Department.Count);
+                }
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("3. Empleados remotos. Estos son los empleados cuya ciudad no se encuentre entre las sedes de su departamento.");
+                System.Threading.Thread.Sleep(1000);
+                var query3 = ctx.Employees
+                    .Where(employee => employee.Department.Cities.Any(city => city.Name == employee.City.Name));
 
-                //4. Listar todos los empleados cuyo aniversario de contratación sea el próximo mes.
-              
+                foreach (var employee in query3)
+                {
+                    Console.WriteLine(employee.FirstName + ' ' + employee.LastName);
+                }
 
-                //Listar los 12 meses del año y el numero de empleados contratados por cada mes.
-              
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("4. Empleados cuyo aniversario de contratación sea el próximo mes.\n");
+                System.Threading.Thread.Sleep(1000);
+                var query4 = ctx.Employees
+                  .Where(e => e.HireDate.Month == System.DateTime.Now.Month + 1);
 
+                foreach (var employee in query4)
+                {
+                    Console.WriteLine("Nombre: {0} {1} Contratado en:  {2}",
+                    employee.FirstName, employee.LastName, employee.HireDate);
+                }
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("5. Listar los 12 meses del año y el numero de empleados contratados por cada mes.\n");
+                System.Threading.Thread.Sleep(1000);
+                var query5 = ctx.Employees
+                    .GroupBy(employee => employee.HireDate.Month)
+                    .OrderBy(i => i.Key)
+                    .Select(res => new { monthNumber = res.Key, Count = res.Count() });
+
+                foreach (var meses in query5)
+                {
+                    Console.WriteLine("Numero de Mes: {0}, Empleados contratados: {1}",
+                     meses.monthNumber, meses.Count);
+                }
             }
-
 
             Console.Read();
         }
+
     }
+
 }
